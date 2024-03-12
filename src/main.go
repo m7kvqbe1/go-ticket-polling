45a34e50b0be80dc738f8b50e87ae166f7c816b6
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2024-03-12 13:15:22 +0000

    Refactor to use Colly for scraping

--- src/main.go
+++ src/main.go
@@ -4,31 +4,29 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-	"sync"
 	"time"
+
+	"github.com/gocolly/colly/v2"
 )
 
 type Config struct {
-	URI          string   `json:"URI"`
+	URL          string   `json:"URL"`
 	SMSKey       string   `json:"SMS_KEY"`
 	PhoneNumbers []string `json:"PHONE_NUMBERS"`
 	IntervalMS   int      `json:"INTERVAL_MS"`
 }
 
-var client = &http.Client{
-	Timeout: time.Second * 30,
-	Transport: &http.Transport{
-		TLSClientConfig:     &tls.Config{InsecureSkipVerify: false}, // It's recommended to not disable SSL verification
-		MaxIdleConns:        10,
-		IdleConnTimeout:     30 * time.Second,
-		TLSHandshakeTimeout: 10 * time.Second,
-	},
-}
+var (
+	httpClient = &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+)
 
 func loadConfig() Config {
 	var config Config
@@ -47,23 +45,23 @@ func loadConfig() Config {
 	return config
 }
 
-func sendText(number string, key string) {
+func sendText(number, key string) {
 	message := `BUY DI TIKITZ!!!`
 	reqBody := strings.NewReader(fmt.Sprintf(`{"phone": "%s", "message": "%s", "key": "%s"}`, number, message, key))
 
 	req, err := http.NewRequest("POST", "https://textbelt.com/text", reqBody)
 
 	if err != nil {
-		log.Println(err)
+		log.Println("Error creating request:", err)
 		return
 	}
 
 	req.Header.Add("Content-Type", "application/json")
 
-	resp, err := client.Do(req)
+	resp, err := httpClient.Do(req)
 
 	if err != nil {
-		log.Println(err)
+		log.Println("Error sending SMS:", err)
 		return
 	}
 
@@ -76,30 +74,40 @@ func sendText(number string, key string) {
 	}
 }
 
-func parsePage(html string, config Config) {
-	match := `<a id="buynow" href="#" title="Buy tickets">Buy tickets</a>`
+func fetch(config Config) {
+	c := colly.NewCollector()
+	foundBuynow := false
 
-	if strings.Contains(html, match) {
+	c.OnHTML("a[id='buynow']", func(e *colly.HTMLElement) {
+		foundBuynow = true
 		success(config)
-	} else {
-		failure()
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		if !foundBuynow {
+			failure()
+		}
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("Error fetching %s: %v\n", r.Request.URL, err)
+	})
+
+	err := c.Visit(config.URL)
+
+	if err != nil {
+		log.Println("Error visiting:", err)
 	}
 }
 
 func success(config Config) {
 	fmt.Println("BUY DI TIKITZ!!!")
 
-	var wg sync.WaitGroup
-
 	for _, number := range config.PhoneNumbers {
-		wg.Add(1)
-		go func(num string) {
-			defer wg.Done()
-			sendText(num, config.SMSKey)
-		}(number)
+		go sendText(number, config.SMSKey)
 	}
 
-	wg.Wait() // Wait for all SMS sending goroutines to finish
+	time.Sleep(5 * time.Second)
 	log.Fatal("Ending the process.")
 }
 
@@ -107,33 +115,12 @@ func failure() {
 	fmt.Println("no tikz found...")
 }
 
-func fetch(config Config) {
-	resp, err := client.Get(config.URI)
-
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := io.ReadAll(resp.Body)
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		bodyString := string(bodyBytes)
-		parsePage(bodyString, config)
-	}
-}
-
 func scrapeLoop(config Config) {
-	for {
-		go fetch(config)
-		time.Sleep(time.Duration(config.IntervalMS) * time.Millisecond)
+	ticker := time.NewTicker(time.Duration(config.IntervalMS) * time.Millisecond)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		fetch(config)
 	}
 }
 
